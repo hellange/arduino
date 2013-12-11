9600715aa0fbdd8236541a5ed9329ddb31f0dc89
hellange <hellange@cisco.com>
2013-12-11 18:51:33 +0100

    Added weatherstation.

---
+++ weatherstation2/weatherstation.h
@@ -0,0 +1,31 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef weatherstation_H_
+#define weatherstation_H_
+#include "Arduino.h"
+//add your includes for the project weatherstation here
+#include <UTFT.h>
+#include <Adafruit_BMP085.h>
+#include <Wire.h>
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project weatherstation here
+
+
+
+
+//Do not add code below this line
+#endif /* weatherstation_H_ */
