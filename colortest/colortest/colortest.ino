862f9876147c1e0f942b021ccd7519fb4a2f197a
ubuntu <helge@ubuntu.(none)>
2014-03-04 12:53:23 -0800

    Refactored.

--- colortest/colortest/colortest.ino
+++ colortest/colortest/colortest.ino
@@ -17,10 +17,11 @@ const int nr = 10;
 const uint32_t row1[nr]  = {0xff0000, 0x00ff00, 0x0000ff, 0x880000, 0x008800, 0x000088, 0x888888, 0xffff00, 0x00ffff, 0xff00ff}; 
 const uint32_t row2[nr] = {0x0060c0, 0xc060e0, 0x003333, 0x009999, 0x104000, 0x001040, 0x004080, 0x404040, 0x440000, 0x004400};
 
-void loop(){
+void drawColorBars(){
+  
   GD.ClearColorRGB(0x104000);
   GD.Clear();
-
+  
   // first row
   for (int i = 0; i<nr; i++){ 
     GD.ColorRGB(row1[i]);
@@ -36,6 +37,12 @@ void loop(){
     GD.Vertex2ii(i*width , height );
     GD.Vertex2ii((i+1)*width, height*2  );
   } 
+}
+
+void loop(){
+
+
+  drawColorBars();
     
   GD.ColorRGB(0x000000);
   GD.cmd_text(240, height / 2, 31, OPT_CENTER, "T e s t i n g   c o l o r s");
