1ab17c3ea1652498ca2899fd4a7f13a2a7d95992
ubuntu <helge@ubuntu.(none)>
2014-03-05 13:33:48 -0800

    Renamed method to reflect that it is no longer only Gameduino based.

--- colortest/colortest.ino
+++ colortest/colortest.ino
@@ -17,7 +17,7 @@ const int bars = 10;
 const uint32_t row1[bars] = {0xff0000, 0x00ff00, 0x0000ff, 0x880000, 0x008800, 0x000088, 0x888888, 0xffff00, 0x00ffff, 0xff00ff}; 
 const uint32_t row2[bars] = {0x0060c0, 0xc060e0, 0x003333, 0x009999, 0x104000, 0x001040, 0x004080, 0x404040, 0x440000, 0x004400};
 
-void drawGD(){
+void drawIt(){
 
   int barWidth = ct.width/bars;
   // first row
@@ -34,7 +34,7 @@ void drawGD(){
 }
 
 void loop(){
-  drawGD();
+  drawIt();
   while (1==1);
 
 }
